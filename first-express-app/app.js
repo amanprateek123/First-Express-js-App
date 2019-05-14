757bb177e977d983275b9c24663d8f2f1d6e8405
Aman Prateek <amanpra333@gmail.com>
2019-05-14 13:41:35 +0530

    sever

--- first-express-app/app.js
+++ first-express-app/app.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get("/",function(req,res){
-res.send("Hello Aman");
-
-});
+app.get("/aman", (req,res) =>res.send("Hello Aman"));
 
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("server is started");
